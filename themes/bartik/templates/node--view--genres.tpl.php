8de5530bb8321c1957949cfe59077bc5d8c62001
Henry-W <henry.white@capgemini.com>
2015-01-09 16:31:50 +0000

    Initial Commit

---
+++ themes/bartik/templates/node--view--genres.tpl.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * @file
+ * Returns the HTML for a node.
+ *
+ * This template is derived from the Zen default node.tpl.php template. It adds:
+ * - Conditional linking of the node title depending on whether the $node_url
+ *   variable is set or not.
+ *
+ * Complete documentation for this file is available online.
+ * @see https://drupal.org/node/1728164
+ */
+?>
+<article class="node-<?php print $node->nid; ?> <?php print $classes; ?> clearfix"<?php print $attributes; ?>>
+
+  <?php print render($title_prefix); ?>
+  <?php if (!$page): ?>
+    <h2<?php print $title_attributes; ?>>
+      <a href="<?php print $node_url; ?>"><?php print $title; ?> - <?php print render($content['field_review_date']); ?></a>
+    </h2>
+  <?php endif; ?>
+  <?php print render($title_suffix); ?>
+
+  <?php
+  hide($content['field_review_date']);
+  hide($content['field_reviewer']);
+  hide($content['field_trailers_external_link']);
+  hide($content['field_in_cinemas_external_link']);
+  hide($content['field_buy_dvd_external_link']);
+  hide($content['field_netflix_external_link']);
+  hide($content['comments']);
+  hide($content['links']);
+  hide($content['field_review_rating']);
+
+  ?>
+<br />
+  <?php print render($content); ?>
+
+  <ul class="group-review-links-teaser">
+    <li><?php print render($content['field_trailers_external_link']); ?> | </li>
+    <li><?php print render($content['field_in_cinemas_external_link']); ?> | </li>
+    <li><?php print render($content['field_buy_dvd_external_link']); ?> | </li>
+    <li><?php print render($content['field_netflix_external_link']); ?></li>
+  </ul>
+
+</article>
